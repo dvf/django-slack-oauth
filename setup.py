79e5551c6d8d3a448c1a0df962e766b82f3b265f
Daniel van Flymen <vanflymen@gmail.com>
2016-12-26 15:54:12 -0500

    Updated packaged

--- setup.py
+++ setup.py
@@ -4,27 +4,28 @@ import os
 from setuptools import setup
 
 
-with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
+with open(os.path.join(os.path.dirname(__file__), 'README.md')) as readme:
     README = readme.read()
 
 
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
-    name='django-slack-oauth',
-    version='0.6.1',
+    name='django-slack-integration',
+    version='1.1',
     packages=[
         'django_slack_oauth',
         'django_slack_oauth.migrations',
     ],
     include_package_data=True,
     license='MIT License',
-    description='Handles OAuth and stores slack token',
+    description='Lightweight OAuth integration with Slack for your Django Projects',
     long_description=README,
-    author='Sergey Keller',
-    author_email='izdieu@gmail.com',
-    url='https://github.com/izdi/django-slack-oauth',
-    download_url='https://github.com/izdi/django-slack-oauth/tarball/0.6.1',
+    author='Daniel van Flymen',
+    author_email='vanflymen@gmail.com',
+    url='https://github.com/vanflymen/django-slack-oauth',
+    download_url='https://github.com/vanflymen/django-slack-oauth/tarball/1.1',
+    keywords=['django', 'slack', 'oauth'],
     install_requires=[
         'Django>=1.8',
         'requests',
